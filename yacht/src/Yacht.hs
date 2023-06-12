fb93a02ae33537f87acd015dc206f77bf364356d
Abhijit Sarkar <contact@asarkar.com>
2023-06-12 15:11:35 -0700

    Simplify yacht

--- yacht/src/Yacht.hs
+++ yacht/src/Yacht.hs
@@ -17,19 +17,15 @@ data Category
   | BigStraight
   | Choice
   | Yacht
+  deriving (Eq, Ord, Enum)
 
 yacht :: Category -> [Int] -> Int
 yacht category dice =
   let f = M.fromList . map (head A.&&& length) . L.group . L.sort $ dice
       cnt k = M.findWithDefault 0 k f
    in case category of
-        Ones -> cnt 1
-        Twos -> cnt 2 * 2
-        Threes -> cnt 3 * 3
-        Fours -> cnt 4 * 4
-        Fives -> cnt 5 * 5
-        Sixes -> cnt 6 * 6
-        FullHouse | M.size f == 2 -> M.foldrWithKey (\k x y -> k * x + y) 0 $ M.filter (\x -> x == 2 || x == 3) f
+        n | n <= Sixes -> let x = 1 + fromEnum n in x * cnt x
+        FullHouse | M.size f == 2 -> M.foldrWithKey (\k v acc -> k * v + acc) 0 $ M.filter (\x -> x == 2 || x == 3) f
         FourOfAKind -> M.foldrWithKey (\k _ _ -> k * 4) 0 $ M.filter (>= 4) f
         LittleStraight | M.size f == 5 && M.notMember 6 f -> 30
         BigStraight | M.size f == 5 && M.notMember 1 f -> 30
