f87e691f7f5b1ccf556a4b86f1a99b7a1ac5c1e3
Abhijit Sarkar <contact@asarkar.com>
2024-10-06 19:20:18 -0700

    Redo spiral matrix

--- spiral-matrix/src/Spiral.hs
+++ spiral-matrix/src/Spiral.hs
@@ -1,24 +1,24 @@
 module Spiral (spiral) where
 
-import Data.List (transpose)
-
--- spiral is the first row plus a smaller spiral rotated 90 deg
-spiral' :: Int -> Int -> Int -> [[Int]]
-spiral' 0 _ _ = [[]]
-spiral' h w s = [s .. s + w - 1] : rot90 (spiral' w (h - 1) (s + w))
-  where
-    rot90 = map reverse . transpose
+import qualified Data.List as L
+import qualified Data.List.Split as LS
 
 spiral :: Int -> [[Int]]
-spiral 0 = []
-spiral size = spiral' size size 1
+spiral n = LS.chunksOf n $ map snd $ L.sort xs
+  where
+    coord = spiral' n 0 0
+    xs = zip coord [1 ..]
 
---
--- How it works
--- spiral' 3 3 1
--- gets expanded into the following
--- [1,2,3]:rot90 ([4,5]:rot90( [6,7]:rot90([8]:rot90 ([9]:[[]]))))
--- so we can conceptually think of this as a spiral of dimension 1 1 starting with 9 (spiral' 1 1 9),
--- appended by a spiral' 2 1 8 etc etc
---
--- Each rot90 rotates the whole tail of the list!
+-- Generates the coordinates of the outer layer in spiral order,
+-- then recurses into the inner matrix.
+spiral' :: Int -> Int -> Int -> [(Int, Int)]
+spiral' n row col
+  | n <= 0 = []
+  | n == 1 = [(row, col)]
+  | otherwise = outer ++ spiral' (n - 2) (row + 1) (col + 1)
+  where
+    topRow = [(row, c) | c <- [col .. (col + n - 1)]]
+    rightCol = [(r, col + n - 1) | r <- [(row + 1) .. (row + n - 1)]]
+    bottomRow = [(row + n - 1, c) | c <- [(col + n - 2), (col + n - 3) .. col]]
+    leftCol = [(r, col) | r <- [(row + n - 2), (row + n - 3) .. (row + 1)]]
+    outer = topRow ++ rightCol ++ bottomRow ++ leftCol
